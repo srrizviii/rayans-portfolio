117743cc5d3c22fd57aa5562a5a8be3e5fd741e1
srrizviii <rayanr0041@gmail.com>
2026-06-03 12:20:11 -0600

    added blurb for old site

--- src/pages/v2/components/Footer.jsx
+++ src/pages/v2/components/Footer.jsx
@@ -2,6 +2,7 @@ export default function Footer() {
     return (
         <footer>
             <p>Built with React using <a href="https://code.visualstudio.com/" target="_blank" className="anchor-text" rel="noreferrer">VS Code</a></p>
+            <p>Check out my <a href="https://rayanrizvi.netlify.app/v1" target="_blank" className="anchor-text" rel="noreferrer">original site</a>!</p>
             <a href="#top-of-page" className="scroll-to-top">
                 <i className="fa-solid fa-arrow-up"></i>
             </a>
